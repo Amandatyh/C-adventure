4ebfa643f27c3dc9d29c4f88f0f1c6856481b50d
huyutong <152272196+Amandatyh@users.noreply.github.com>
2023-12-17 17:24:00 +0800

    Add files via upload

---
+++ 2.2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("These values are:\n");
+	float x=2.5,y=2.5,z=2.5;
+	printf("x=%.1f\n",x);
+	printf("y=%.1f\n",y);
+	printf("z=%.1f\n",z);
+    return 0;
+}
